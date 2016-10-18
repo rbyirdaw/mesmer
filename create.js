dcaa43e441aaeae5ac5e2728ab9ce7ce568fdba0
rbyirdaw <yrobelb@gmail.com>
2016-10-18 17:28:43 -0500

    reorg, refacator

--- create.js
+++ create.js
@@ -7,7 +7,12 @@ function init() {
     _vis = {
         resData: [],
 	pwDist: [],
-	pwDistMin: 12,
+	pwDistMin: 6,
+	nodes: [],
+	edges: [],
+	force: undefined,
+//        node: undefined,
+//        link: undefined,
 	numRes: undefined
     };
 
@@ -17,46 +22,12 @@ $(document).ready(function() {
 		_vis.pwDistMin = this.value;
 		$(".pwDistMinDisplay").html(this.value);
 		console.log(this.value);
+
 	});
   });
 }
 //=============================================================================
 
-/*
-function rand_pw_distances() {
-
-  for (var i = 0; i < numRes; i++) {
-    pw_distances[i] = [];
-    tempStr = "";
-    for (var j = (i + 1); j < numRes; j++) {
-      pw_distances[i][j] = 1 + Math.floor(Math.random() * 12);
-      tempStr += (pw_distances[i][j] + " ");
-    }
-    console.log(tempStr);
-  }
-
-}
-*/
-//=============================================================================
-function calc_pw_distances() {
-
-  for (var i = 0; i < _vis.numRes; i++) {
-    _vis.pwDist[i] = [];
-    tempStr = "";
-    for (var j = (i + 1); j < _vis.numRes; j++) {
-      _vis.pwDist[i][j] = Math.sqrt(
-				Math.pow( (_vis.resData[i].x - _vis.resData[j].x), 2 ) +
-				Math.pow( (_vis.resData[i].y - _vis.resData[j].y), 2) + 
-				Math.pow( (_vis.resData[i].z - _vis.resData[j].z), 2) 
-				).toFixed(2);
-      tempStr += (_vis.pwDist[i][j] + " ");
-    }
-    //console.log(tempStr);
-  }
-
-}
-
-//=============================================================================
 function createDisplay(fileName) {
 
   d3.csv(fileName,
@@ -72,8 +43,10 @@ function createDisplay(fileName) {
 	build_primary_sequence_display();
 
 	calc_pw_distances();
+	getResNodesEdges();
 
 	build_force_layout();
+	setNodesLinks();
       });
 	
 }//read_pdb_csv
