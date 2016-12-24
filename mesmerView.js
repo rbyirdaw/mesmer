da40edab52a9cfa0f68040738e0e51c6180fc316
rbyirdaw <yrobelb@gmail.com>
2016-12-23 23:04:27 -0600

    begin force layout logic on view

--- mesmerView.js
+++ mesmerView.js
@@ -7,7 +7,27 @@
     this.structureSelect = document.getElementsByName("structureSelect");
     this.pwDistSelect = document.getElementsByName("pwDistSelect");
     this.resPairGapSelect = document.getElementsByName("resPairGapSelect");
+	
+	this.forceLayout = {
+	  force: undefined,
+      nodes: undefined,
+      links: undefiend,
 
+      color: d3.scale.category20();	  
+	};
+
+  }
+  
+  MesmerView.prototype.create = function() {
+	
+    this.forceLayout.force = d3.layout.force()
+        .nodes(this.forceLayout.nodes)
+		.linkDistance([150])
+		.charge([-150])
+		.on("tick", tick);
+		
+    var node = d3.select("svg").selectAll(".g-node");		
+	  
   }
 
   MesmerView.prototype.setListener = function(action, eveHandler) {
@@ -22,7 +42,8 @@
   };
 
   MesmerView.prototype.setNodes = function(nodes) {
-
+    
+	this.forceLayout.nodes = nodes;
 
   };
 
