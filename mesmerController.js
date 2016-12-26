9b0cee3f0c88467a6930baa6db92a9e5b0e61e20
rbyirdaw <yrobelb@gmail.com>
2016-12-25 18:24:09 -0600

    force layout plotting nodes

--- mesmerController.js
+++ mesmerController.js
@@ -7,7 +7,83 @@
     this.model = model;
     this.view = view;
 
+    var self = this;
+    this.view.setListener("loadStructure", function(fileName) {
+	self.loadStructure(fileName);
+    });
+
   }
+  
+//==============================================================================  
+
+  MesmerController.prototype.loadStructure = function(value) {
+    console.log("at controller "+value);
+
+    var data = undefined;
+	var self = this;
+
+    d3.text("coordinates/"+value, function(text) {
+
+      data = d3.csv.parse(text, function(d) {
+	    console.log(d);
+		return {resNum: +d.resNum, resID: d.resID, x: +d.x, y: +d.y, z: +d.z} ;
+      });
+
+    var pwDist = self.model.getPWdistances();
+
+    var edges = self.getEdges(pwDist);
+    self.view.setEdges(edges);
+
+    var nodes = self.getNodes(data);
+    self.view.setNodes(nodes);
+		
+	self.model.create(data);
+	
+	self.view.create();	  	
+	  
+    });//d3.text
+
+  };
+
+  MesmerController.prototype.getEdges = function(pwDist) {
+  
+    var i, j;
+    var edges = [];
+    var numRes = pwDist.length;
+
+    for (i = 0; i < numRes; i++) {
+
+      for (j = i; j < numRes; j++) {
+        
+        if ( (j !== i) &&
+	    (Math.abs(i - j) >= this.resPairGapMin) &&
+	    (pwDist[i][j] <= this.pwDistMax) ) {
+
+	  edges.push({source: i, target: j, "distance": pwDist[i][j]});
+	}
+
+      }//j
+
+    }//i
+
+    return edges;
+
+  };
+
+
+  MesmerController.prototype.getNodes = function(data) {
+
+    var i;
+    var nodes = [];
+
+    for (i = 0; i < data.length; i++) {
+      nodes.push({resNum: data[i].resNum, resID: data[i].resID});
+    }
+
+    return nodes;
+
+  };
+
 
   window.mesmerApp = window.mesmerApp || {};
   window.mesmerApp.Controller = MesmerController;
--- mesmerView.js
+++ mesmerView.js
@@ -4,6 +4,100 @@
 
   function MesmerView() {
 
+    this.structureSelect = document.getElementsByName("structureSelect");
+    this.pwDistSelect = document.getElementsByName("pwDistSelect");
+    this.resPairGapSelect = document.getElementsByName("resPairGapSelect");
+	
+	this.forceLayout = {
+	  force: undefined,
+      nodes: undefined,
+      links: undefined,
+
+      color: d3.scale.category20()  
+	};
+
+  }
+
+//==============================================================================
+  
+  MesmerView.prototype.create = function() {
+	
+	var self = this;
+	
+    self.forceLayout.force = d3.layout.force()
+		.size([640, 480])
+        .nodes(self.forceLayout.nodes)
+		.linkDistance([150])
+		.charge([-150])
+		.on("tick", self.tick);
+	
+    var node = d3.select("svg").selectAll(".g-node");
+	node = node.data(self.forceLayout.nodes);
+
+	var nodeEnter = node.enter()
+		.append("g")
+		.attr("class", "g-node")
+		.call(self.forceLayout.force.drag);
+
+	nodeEnter.append("circle")		
+		.attr("r", 26)
+		.attr("fill", function(d) { 
+		  return self.forceLayout.color(d.resNum);
+		});
+
+	nodeEnter.append("text")
+	    .attr("dy",".35em")
+	    .attr("text-anchor", "middle")
+	    .text(function(d) { 
+	      return d.resID+"\n"+(d.resNum+1);
+	    });
+
+	node.exit().remove();	
+	
+
+    self.forceLayout.force.start();	
+  }
+
+  
+  MesmerView.prototype.setListener = function(action, eveHandler) {
+
+    if (action === "loadStructure") {
+      this.structureSelect[0].addEventListener("change", function() {
+	console.log(this.value);
+        eveHandler(this.value);
+      }, false);
+
+    }
+  };
+
+  
+  MesmerView.prototype.setNodes = function(nodes) {
+    
+	this.forceLayout.nodes = nodes;
+
+  };
+
+
+
+  MesmerView.prototype.setEdges = function(edges) {
+
+
+  };
+  
+  MesmerView.prototype.tick = function() {
+    var node = d3.select("svg").selectAll(".g-node");
+
+    node.selectAll("circle")
+		.attr("cx", function(d) { return d.x; })
+		.attr("cy", function(d) { return d.y; });
+	node.selectAll("text")
+		.attr("x", function(d) {
+          return d.x;
+        })
+        .attr("y", function(d) {
+          return d.y;
+        });
+	
   }
 
   window.mesmerApp = window.mesmerApp || {};
