95cbd6a82b59e236c76d09d51c5aef3b6852a567
rbyirdaw <yrobelb@gmail.com>
2020-12-18 19:35:21 -0600

    webpack config

---
+++ webpack-dev.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    open: true
+  }
+};
+
+module.exports = config;
