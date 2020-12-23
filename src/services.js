8f7d4d28630d0b2cbe3cc9ee7638e66cd03391f2
rbyirdaw <yrobelb@gmail.com>
2020-12-22 19:37:37 -0600

    Implement pdb and structure search

--- src/services.js
+++ src/services.js
@@ -1,17 +1,48 @@
 
-const pdbSearchApi = `https://search.rcsb.org/rcsbsearch/v1/query`;
+const pdbSearchApi = `https://search.rcsb.org/rcsbsearch/v1/query?json=`;
 const modelServerApi = `https://models.rcsb.org/v1/`;
 
-const httpClient = () => {
-  
+const httpClient = (url, requestOptions) => {
+  return fetch(url, requestOptions);  
 };
 
 export const searchPdbEntry = (searchValue) => {
 
+  let encodedSearchValue = encodeURI(JSON.stringify(searchValue));
+  //let response = await httpClient(pdbSearchApi + `%7B"query":%7B"type":"terminal","service":"text","parameters":%7B"value":"thymidine%20kinase"%7D%7D,"return_type":"entry"%7D`);
+
+  return new Promise((resolve, reject) => {
+    httpClient(pdbSearchApi + encodedSearchValue)
+      .then(response => {
+        const contentType = response.headers.get('content-type');
+        if (!response.ok || !contentType || !contentType.includes('application/json')) {
+          reject("Response error.")
+        } else {
+          resolve(response.json());
+        }
+      });
+  });
+
 };
 
-export const getProteinStructure = (pdbId = '2LZM') => {
-  const searchParams = `${pdbId}/full?encoding=cif&copy_all_categories=false`;
+export const getProteinStructure = (pdbId) => {
+ 
+  return new Promise((resolve, reject) => {
+    if (!pdbId) {
+      reject("Invalid input.")
+    } else {
+      pdbId = pdbId.toLowerCase();
+      const searchParams = `${pdbId}/full?encoding=cif&copy_all_categories=false`;
+      httpClient(modelServerApi + searchParams)
+        .then(response => {
+          if (!response.ok) {
+            reject("Response error.");
+          } else {
+            resolve(response.text());
+          }
+        });
+      }
+  });
 
 };
 
