55d02baf459e1dfc8ead6259c53c270a0772cb33
rbyirdaw <yrobelb@gmail.com>
2021-02-19 19:39:10 -0600

    Reworked d3-graph to be able to update nodes and links

--- src/components/d3-graph.js
+++ src/components/d3-graph.js
@@ -22,24 +22,25 @@ export class D3Graph extends HTMLElement {
     this.shadowRootRef = this.attachShadow({mode: 'open'});
     this.shadowRootRef.appendChild(componentHtml.content.cloneNode(true));
     this.svg;
-    this.width = 960;
-    this.height = 600;
+    this.width = this.getAttribute('width');
+    this.height = this.getAttribute('height');
     this.node;
     this.link;
-    this._nodes;
-    this._links;
+    this._nodes = [];
+    this._links = [];
     this.simulation;
   }
 
   connectedCallback() {
     this.shadowRootRef.querySelector('#d3-graph-container')
-      .innerHTML = `<svg width=${this.width} height=${this.height}></svg>`;
+      .innerHTML = `<svg width=${this.getAttribute('width')} height=${this.getAttribute('height')}></svg>`;
 
     this._nodes = [
-      {"id": "A"},
-      {"id": "B"},
-      {"id": "C"}
+      {id: "A"},
+      {id: "B"},
+      {id: "C"}
     ];
+
 
     this._links = [
       {"source": 0, "target": 1},
@@ -48,61 +49,93 @@ export class D3Graph extends HTMLElement {
 
     if (d3) {
       this.svg = d3.select(this.shadowRootRef.querySelector('svg'));
-      this.simulation = d3.forceSimulation();
+      this.svg.append("g")
+        .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")");
+
+      this.simulation = d3.forceSimulation(this._nodes)
+        .force("charge", d3.forceManyBody().strength(-1000))
+        .force("link", d3.forceLink(this._links).distance(50))
+        .force("x", d3.forceX())
+        .force("y", d3.forceY())
+        .alphaTarget(1)
+        .on("tick", this.ticked.bind(this));
+      
       this.createNodeElements();
       this.createLinkElements();
-      this.setSimulationNodes();
-      this.setSimulationLinks();
+      this.joinNodes();
+      this.joinLinks();
+
+      this.render();
     }
   }
 
   set nodes(nodeList) {
-    this._nodes = nodeList;
+    this._nodes = nodeList;  
+    this.joinNodes();
+
+    //clear links?
+    this._links = [];
+    this.joinLinks();
+
+    //update view
+    this.render();
   }
 
 
   set links(linkList) {
-    this._links = linkList;
+    this._links = linkList;    
+    this.joinLinks();
+    this.render();      
+  }
+
+  updateGraph(nodesLinks) {
+    // nodesLinks = {nodes: [{},{},...], links: [{},{},...]}
+    const {nodes, links} = nodesLinks;
+    this._nodes = nodes;
+    this._links = links;
+
+    this.joinNodes();
+    this.joinLinks();
+
+    this.render();
   }
 
   createNodeElements() {
-    const n = this._nodes;
-    this.node = this.svg.append('g')
-      .attr('class', 'nodes')
-      .selectAll('circle')
-      .data(n)
-      .enter().append('circle')
-        .attr('r', 2.5)
-        .call(d3.drag()
-          .on("start", this.dragstarted.bind(this))
-          .on("drag", this.dragged.bind(this))
-          .on("end", this.dragended.bind(this)))
+    this.node = this.svg.select("g")
+      .append('g')
+        .attr('class', 'nodes')
+        .selectAll('circle');
   }
 
   createLinkElements() {
-    this.link = this.svg.append('g')
-      .attr('class', 'links')
-      .selectAll('line')
-      .data(this._links)
-      .enter().append('line');
+    this.link = this.svg.select("g")
+      .append('g')
+        .attr('class', 'links')
+        .selectAll('line');
   }
 
-  setSimulationNodes() {
-    this.simulation
-      .nodes(this._nodes)
-      .on('tick', this.ticked.bind(this));    
+  joinNodes() {
+    this.node = this.node
+      .data(this._nodes, d => d.id)
+      .join(enter => enter.append("circle")
+        .attr("r", 8)
+        .attr("fill","aqua")
+        .call(d3.drag()
+          .on("start", this.dragstarted.bind(this))
+          .on("drag", this.dragged.bind(this))
+          .on("end", this.dragended.bind(this)))        
+        );    
   }
 
-  setSimulationLinks() {
-    this.simulation
-      .force('link', d3.forceLink().links(this._links))
-      .force('charge', d3.forceManyBody().strength(-20))
-      .force('center', d3.forceCenter(this.width/2, this.height/2));
+  joinLinks() {
+    this.link = this.link
+      .data(this._links, d => [d.source, d.target])
+      .join("line");    
   }
 
   ticked() {
     try {
-      this.link
+      this.link && this.link
       .attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
@@ -133,5 +166,10 @@ export class D3Graph extends HTMLElement {
     if (!d3.event.active) this.simulation.alphaTarget(0);
     d.fx = null;
     d.fy = null;
+  }
+
+  render() {
+    this.simulation.nodes(this._nodes);
+    this.simulation.force("link").links(this._links);
   }
 }
