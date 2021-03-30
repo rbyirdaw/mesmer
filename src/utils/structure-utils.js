3480e494dc16a0c950372c6a6a756e129222da12
rbyirdaw <yrobelb@gmail.com>
2021-03-30 01:28:31 -0500

    Added residue pairwise distance calculation

--- src/utils/structure-utils.js
+++ src/utils/structure-utils.js
@@ -1,3 +1,4 @@
+import { line } from "d3-shape";
 
 export const getAtomLines = (fullStructure) => {
   // Given a full structure from modelServer API,
@@ -37,4 +38,51 @@ export const getResidues = (atomLinesArray) => {
   }
 
   return residues;
+};
+
+export const getCoordinates = (atomLinesArray) => {
+  // Given array of atomic coordinates, from full structure
+  // i.e., "ATOM 54 C C TRP 4 . . A 1 -4.328 -2.716 -0.248 1 0 ? C TRP 4 A 1 ",
+  // return array of coordinates as [{.x1, .y1, .z1}, {.x2, .y2, .z2}, ... {.xN, .yN, .zN}]
+
+  let coords = [];
+  if (atomLinesArray.length > 0) {
+    coords = atomLinesArray.map((el) => {
+      const lineArray = el.split(' ');
+      const x = lineArray[10];
+      const y = lineArray[11];
+      const z = lineArray[12];
+      
+      return {x: x, y: y, z: z};
+    });
+  }
+
+  return coords;
+
+};
+
+export const calcPairwiseDistances = (coords) => {
+  // Given an array of 3D coordinates for 2 or more residues,
+  // i.e. [{.x1, .y1, .z1}, {.x2, .y2, .z2}, ... {.xN, .yN, .zN}]
+  // calculate pairwise distances.
+  
+  let pairwiseDist = [];
+  if (coords && coords.length > 1) {
+    const numObjects = coords.length;
+
+    for (let i = 0; i < numObjects - 1; i++) {
+      const obj1 = coords[i];
+      let singleDist = [];
+      for (let j = i + 1; j < numObjects; j++) {
+        const obj2 = coords[j];
+        const dist = Math.sqrt(Math.pow((obj2.x - obj1.x), 2) +
+            Math.pow((obj2.y - obj1.y), 2) + 
+            Math.pow((obj2.z - obj1.z), 2));
+        singleDist.push(dist);
+      }
+      pairwiseDist.push(singleDist);
+    }
+  }
+
+  return pairwiseDist;
 }
