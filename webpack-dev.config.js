8e4b2eee22deec0334f44e59707503d56c86d5b1
rbyirdaw <yrobelb@gmail.com>
2021-06-04 01:13:37 -0500

    Added Babel

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -19,7 +19,17 @@ const config = {
           'style-loader',
           'css-loader'
         ]
-      }
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }      
     ]
   }
 };
