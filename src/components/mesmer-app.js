f695ef4d80ddba594b6f869cb0c3b384e68a594f
rbyirdaw <yrobelb@gmail.com>
2021-09-22 18:51:03 -0500

    Add spinner and alert elements (note css); begin setup for structure component

--- src/components/mesmer-app.js
+++ src/components/mesmer-app.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { SpinnerElement } from './spinner-element';
 import { AlertElement } from './alert-element';
 import MesmerProteinStructure from './mesmer-protein-structure';
@@ -7,6 +6,7 @@ import MesmerControls from './memser-controls';
 
 let appHtml = document.createElement('template');
 appHtml.innerHTML = `
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
   <alert-element></alert-element>
   <spinner-element></spinner-element>
   <mesmer-protein-structure></mesmer-protein-structure>
@@ -28,8 +28,25 @@ export default class MesmerApp extends HTMLElement {
 
   }
 
-  connectedCallback() {
+  connectedCallback() {    
+    const spinnerEl = this.shadowRootRef.querySelector('spinner-element');
 
+    const mesmerStruct = this.shadowRootRef.querySelector('mesmer-protein-structure');
+    mesmerStruct.addEventListener('xhr-state', (e) => {
+      spinnerEl.enabled = e.detail.value;
+      console.log("mesmer struct ", e.detail.value)
+    });
+
+    mesmerStruct.addEventListener('pdb-search-error', (e) => {
+      this.showAlert('error', 'Pdb search error.');
+    });
+  }
+
+  showAlert = (alertType, alertText) => {
+    const alertEl = this.shadowRootRef.querySelector('alert-element');
+    alertEl.alertText = alertText;
+    alertEl.alertType = alertType;
+    alertEl.enabled = true;  
   }
 
 }
